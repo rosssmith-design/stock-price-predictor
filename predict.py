52c5f07af1df8570c6856e8f5edaa510914f2160
Ross Smith <RossSmith0603@gmail.com>
2026-06-15 18:28:50 +0100

    as well as lag features, I have added rolling averages

--- predict.py
+++ predict.py
@@ -9,6 +9,10 @@ df['yesterday'] = df['Close'].shift(1)
 df['2_days_ago'] = df['Close'].shift(2)
 df['3_days_ago'] = df['Close'].shift(3)
 
+# Rolling averages
+df['5_day_avg'] = df['Close'].rolling(5).mean()
+df['20_day_avg'] = df['Close'].rolling(20).mean()
 
 
-print(df[['Close', 'yesterday', '2_days_ago', '3_days_ago']].head(10))
+
+print(df[['Close', 'yesterday', '5_day_avg', '20_day_avg']].head(25))
