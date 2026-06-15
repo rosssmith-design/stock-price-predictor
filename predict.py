d658f0ec0327be30c34440c06020175a05244842
Ross Smith <RossSmith0603@gmail.com>
2026-06-15 18:45:45 +0100

    added momentum features like volume_change using percentage change

--- predict.py
+++ predict.py
@@ -13,6 +13,10 @@ df['3_days_ago'] = df['Close'].shift(3)
 df['5_day_avg'] = df['Close'].rolling(5).mean()
 df['20_day_avg'] = df['Close'].rolling(20).mean()
 
+# Momentum features
+df['volume_change'] = df['Volume'].pct_change()
+df['price_change'] = df['Close'].pct_change()
 
 
-print(df[['Close', 'yesterday', '5_day_avg', '20_day_avg']].head(25))
+
+print(df[['Close', 'volume_change', 'price_change']].head(25))
